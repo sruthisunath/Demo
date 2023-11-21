e6a89f92122d00f0b31279eda7a20871951002c9
sruthi <csruthi378@gmail.com>
2023-11-21 15:00:04 +0530

    project

---
+++ webscraper_project/websitescraper_app/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from .models import Links
+# Create your views here.
+def home(request):
+    if request.method=="POST":
+        link_new=request.POST.get('page','')
+        urls=requests.get(link_new)
+        beautysoup=BeautifulSoup(urls.text,'html.parser')
+
+        for link in beautysoup.findAll('a'):
+            li_address=link.get('href')
+            li_name=link.stripped_string
+            Links.objects.create(address=li_address,stringname=li_name)
+        return HttpResponseRedirect('/')
+    else:
+        data_values=Links.objects.all()
+    return render(request,'home.html',{'data_values':data_values})
